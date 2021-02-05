9e2ac45d1207f5914797b5a1d3dd8d2f206f87d0
Olaoluwa Raji <46250887+MUDAL@users.noreply.github.com>
2021-02-05 13:26:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,14 @@
 
 static uint8_t Hex_To_BCD(uint8_t hex)
 {
-	uint8_t bcd = hex;
+	uint8_t bcd = 0;
 	uint8_t multipleOfTen = 0;
-	while (bcd >= 10)
+	while (hex >= 10)
 	{
-	    bcd -= 10;
+	    hex -= 10;
 	    multipleOfTen++;
 	}
-	bcd = (multipleOfTen<<4) | bcd;
+	bcd = (multipleOfTen<<4) | hex;
 	return bcd;
 }
 
